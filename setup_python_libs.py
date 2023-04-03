269ffe815efef24428368ec896a9cafafe0ffe58
Andrew Chin <39199125+ivlzme@users.noreply.github.com>
2023-04-03 16:06:52 -0400

    Update setup_python_libs.py

--- setup_python_libs.py
+++ setup_python_libs.py
@@ -11,3 +11,14 @@ sys.path.append('/usr/lib/python3/dist-packages')
 sys.path.append('/usr/local/lib/python3.8/dist-packages')
 username = pwd.getpwuid(os.getuid())[0]
 sys.path.append('/home/{}/.local/python3.8/site-packages'.format(username))
+
+# attempt to make this more portable, but I forgot Ghidra uses jython which is python 2.7
+#import subprocess
+#script="""import site
+#for dir in site.getsitepackages():
+#    print dir
+#"""
+#res = subprocess.run(['python3', '-c', script], stdout=subprocess.PIPE)
+#pkgs = res.stdout.decode('utf-8').split()
+#for pkg in pkgs:
+#    sys.append(pkg)
